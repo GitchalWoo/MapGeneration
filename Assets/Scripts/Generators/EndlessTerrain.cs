8219c4d780c5506d01c74f3e5606de50f6312446
Michal Walczak <michal.pawel.walczak@outlook.com>
2022-05-05 17:28:09 +0200

    Added mesh collider

--- Assets/Scripts/Generators/EndlessTerrain.cs
+++ Assets/Scripts/Generators/EndlessTerrain.cs
@@ -85,6 +85,7 @@ public class EndlessTerrain : MonoBehaviour
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
+        MeshCollider meshCollider;
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
@@ -102,6 +103,7 @@ public class EndlessTerrain : MonoBehaviour
             meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshCollider = meshObject.AddComponent<MeshCollider>();
 
             meshRenderer.material = material;
 
@@ -155,6 +157,7 @@ public class EndlessTerrain : MonoBehaviour
                 {
                     previousLODIndex = lodIndex;
                     meshFilter.mesh = lodMesh.mesh;
+                    meshCollider.sharedMesh = lodMesh.mesh;
                 }
                 else if (!lodMesh.hasRequestedMesh)
                 {
