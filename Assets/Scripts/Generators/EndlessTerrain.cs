395929518f1b92047d38170f8554deae43f09231
Michal Walczak <michal.pawel.walczak@outlook.com>
2022-05-05 14:12:32 +0200

    Added Scale factor implementation

--- Assets/Scripts/Generators/EndlessTerrain.cs
+++ Assets/Scripts/Generators/EndlessTerrain.cs
@@ -6,6 +6,8 @@ using System.Linq;
 public class EndlessTerrain : MonoBehaviour
 {
 
+
+    const float scale = 50f;
     const float viewerMoveThresholdForChunkUpdate = 25f;
     const float sqrtviewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -33,7 +35,7 @@ public class EndlessTerrain : MonoBehaviour
 
     void Update()
     {
-        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
         if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrtviewerMoveThresholdForChunkUpdate)
         {
             viewerPositionOld = viewerPosition;
@@ -103,8 +105,9 @@ public class EndlessTerrain : MonoBehaviour
 
             meshRenderer.material = material;
 
-            meshObject.transform.position = positionV3;
+            meshObject.transform.position = positionV3 * scale;
             meshObject.transform.parent = parent;
+            meshObject.transform.localScale = Vector3.one * scale;
             SetVisible(false);
 
             lodMeshes = new LODMesh[detailLevels.Length];
